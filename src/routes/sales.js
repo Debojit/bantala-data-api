f573d407103d5a87fc0cd5358e400fb1403ed89c
Debojit Sinha <sinha.debojit@gmail.com>
2022-09-08 07:45:18 +0000

    Controller file refactor

--- src/routes/sales.js
+++ src/routes/sales.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 
-const {upload, salesFile} = require('../controllers/upload');
+const {upload, salesFile} = require('../controllers/sales');
 
 const router = express.Router();
 
